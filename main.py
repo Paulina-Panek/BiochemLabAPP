762473ecbcafd9effc3df2f7d4b80323f1051a00
Paulina <ppanek@hawaii.edu>
2019-09-03 16:30:01 -1000

    second screen

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import kivy
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
-class MyGrid(Widget):
+
+class MainWindow(Screen):
     sequence = ObjectProperty(None)
 
     def btn(self):
         print("sequence: ", self.sequence.text)
 
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+
 class MyApp(App): # <- Main Class
     def build(self):
-        return MyGrid()
-
+        return kv
 
 if __name__ == "__main__":
     MyApp().run()
