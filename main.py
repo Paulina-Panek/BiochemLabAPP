af49ba8a1ea4b5f312e3a02f88d3886e0e39b65e
Paulina <ppanek@hawaii.edu>
2019-09-05 17:48:11 -1000

    biopython

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import kivy
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-
+import Bio
 
 
 class MainWindow(Screen):
