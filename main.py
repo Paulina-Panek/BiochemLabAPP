400bda31b95ebdb8594c23879e8ee8d81eb15ee1
Paulina <ppanek@hawaii.edu>
2019-09-12 14:11:50 -1000

    implementing BLAST search...

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
 
 class MainWindow(Screen):
     sequence = ObjectProperty(None)
@@ -12,9 +13,10 @@ class MainWindow(Screen):
         print("sequence: ", self.sequence.text)   #prints input on terminal(internal check)
 
 class SecondWindow(Screen):
-    def blastsearch(self):
+    def blastsearch(self):  ## takes fasta file, runs BLAST search over internet
         fasta_string = open("sequence.fasta").read()
-        result_handle = NCBIWWW.qblast("blastp", "pdb", fasta_string )
+        result_handle = NCBIWWW.qblast("blastp", "nr", fasta_string )  ##using database nr in blastp
+        blast_record = NCBIXML.read(result_handle)
 
 class WindowManager(ScreenManager):
     pass
